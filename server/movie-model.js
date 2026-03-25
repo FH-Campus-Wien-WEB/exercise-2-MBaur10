58cdb78e8cd5bd120f01cfcc20f08748f9dedfee
micha <michaela.baur@stud.hcw.ac.at>
2026-03-25 20:25:12 +0100

    update GET /movies in server.js (task1.2)

--- server/movie-model.js
+++ server/movie-model.js
@@ -1,2 +1,88 @@
-/* Task 1.1. Add your movie data here 
-   and export it so it's available in server.js */
+const movies = {
+   "tt0892769": {
+      imdbID: "tt0892769",
+      Title: "How to Train Your Dragon",
+      Released: "2010-03-26",
+      Runtime: 98,
+      Genres: [
+         "Animation",
+         "Action",
+         "Adventure"
+      ],
+      Directors: [
+         "Dean DeBlois",
+         "Chris Sanders"
+      ],
+      Writers: [
+         "William Davies",
+         "Dean DeBlois",
+         "Chris Sanders"
+      ],
+      Actors: [
+         "Jay Baruchel",
+         "Gerard Butler",
+         "Christopher Mintz-Plasse"
+      ],
+      Plot: "A hapless young Viking who aspires to hunt dragons becomes the unlikely friend of a young dragon himself, and learns there may be more to the creatures than he assumed.",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMjA5NDQyMjc2NF5BMl5BanBnXkFtZTcwMjg5ODcyMw@@._V1_SX300.jpg",
+      Metascore: 75,
+      imdbRating: 8.1
+   },
+   "tt0119698": {
+      imdbID: "tt0119698",
+      Title: "Princess Mononoke",
+      Released: "1997-07-12",
+      Runtime: 133,
+      Genres: [
+         "Animation",
+         "Adventure",
+         "Fantasy"
+      ],
+      Directors: [
+         "Hayao Miyazaki"
+      ],
+      Writers: [
+         "Hayao Miyazaki"
+      ],
+      Actors: [
+         "Yôji Matsuda",
+         "Yuriko Ishida",
+         "Yûko Tanaka"
+      ],
+      Plot: "While seeking to cure himself of a curse, young warrior Ashitaka stumbles into a conflict between the people of Iron Town and Princess Mononoke, a girl raised by wolves, who will stop at nothing to prevent the destruction of her home.",
+      Poster: "https://tse2.mm.bing.net/th/id/OIP.IXsRXiOQLytlNifQuBYlSwHaLH?pid=Api",
+      Metascore: 78,
+      imdbRating: 8.3
+   },
+   "tt0167260": 
+   {
+      imdbID: "tt0167260",
+      Title: "The Lord of the Rings: The Return of the King",
+      Released: "2003-12-17",
+      Runtime: 201,
+      Genres: [
+         "Adventure",
+         "Drama",
+         "Fantasy"
+      ],
+      Directors: [
+         "Peter Jackson"
+      ],
+      Writers: [
+         "J.R.R. Tolkien",
+         "Fran Walsh",
+         "Philippa Boyens"
+      ],
+      Actors: [
+         "Elijah Wood",
+         "Viggo Mortensen",
+         "Ian McKellen"
+      ],
+      Plot: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMTZkMjBjNWMtZGI5OC00MGU0LTk4ZTItODg2NWM3NTVmNWQ4XkEyXkFqcGc@._V1_SX300.jpg",
+      Metascore: 94,
+      imdbRating: 9.0
+   }
+};
+
+module.exports = movies;
--- server/server.js
+++ server/server.js
@@ -15,22 +15,25 @@ app.use(express.static(path.join(__dirname, 'files')));
 app.get('/movies', function (req, res) {
   /* Task 1.2. Remove the line below and eturn the movies from 
      the model as an array */
-  res.sendStatus(404)
-})
+  const moviesArray = Object.values(movieModel);
+  res.json(moviesArray);
+});
 
 // Configure a 'get' endpoint for a specific movie
 app.get('/movies/:imdbID', function (req, res) {
   /* Task 2.1. Remove the line below and add the 
     functionality here */
   res.sendStatus(404)
-})
+});
 
 /* Task 3.1 and 3.2.
    - Add a new PUT endpoint
    - Check whether the movie sent by the client already exists 
      and continue as described in the assignment */
 
-app.listen(3000)
+app.listen(3000);
 
-console.log("Server now listening on http://localhost:3000/")
+console.log("Server now listening on http://localhost:3000/");
 
+console.log("Movies loaded: ", Object.keys(movieModel));
+console.log("Total movies: ", Object.keys(movieModel).length);
